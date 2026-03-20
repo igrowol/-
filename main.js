694da4f2b80e0a7e2d38046b62bd052c9cfff38b
Pemoqo <pemoqo@gmail.com>
2026-03-21 00:54:10 +0300

    newcommitnewversion

--- main.js
+++
@@ -1,9 +0,0 @@
-// js test
-
-const catGirlImage = document.getElementById("catGirlImage")
-const clickMeText = document.getElementById("clickMeText")
-
-catGirlImage.addEventListener('click', function(event) {
-    console.log("works")
-    clickMeText.textContent = 'ybeisa chort'
-});
